c56966d9e33079de5027a9fe4395d194684e1612
junior272006 <junior27nguetta@gmail.com>
2025-12-06 23:36:23 +0000

    vercel

--- frontend/vitrine/src/main.tsx
+++ frontend/vitrine/src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home.tsx';
 import Inscription from './pages/Register.tsx';
 import { theme } from './theme.ts';
@@ -10,26 +11,27 @@ import Dashboard from './pages/Dashboard.tsx';
 import AdminLogin from './pages/admin/Login.tsx';
 import AdminDashboard from './pages/admin/Dashboard.tsx';
 import ProtectedAdminRoute from './components/admin/PrivateRoute.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MantineProvider theme={theme} >
-      <Router>
+    <MantineProvider theme={theme}>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
-            <Route path="/register" element={<Inscription/>} />
-            <Route path="/dashboard" element={<Dashboard/>} />
-<Route path="/admin/login" element={<AdminLogin/>} />
-  <Route 
-          path="/admin/dashboard" 
-          element={
-            <ProtectedAdminRoute>
-              <AdminDashboard />
-            </ProtectedAdminRoute>
-          } 
-        />
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Inscription />} />
+          <Route path="/dashboard" element={<Dashboard />} />
 
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route 
+            path="/admin/dashboard" 
+            element={
+              <ProtectedAdminRoute>
+                <AdminDashboard />
+              </ProtectedAdminRoute>
+            }
+          />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </MantineProvider>
   </StrictMode>
 );
